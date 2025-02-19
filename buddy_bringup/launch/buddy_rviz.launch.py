eeb811af56b553b8a03a62bc4bf7f5b269aa735d
Iesus Davila <iesusdavila@gmail.com>
2025-02-19 08:35:25 -0500

    Robot Bringup

---
+++ buddy_bringup/launch/buddy_rviz.launch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf_file_name = 'buddy_description.urdf'
+
+    print("urdf_file_name : {}".format(urdf_file_name))
+
+    urdf = os.path.join(
+        get_package_share_directory('buddy_description'),
+        'urdf',
+        urdf_file_name)
+
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    rsp_params = {'robot_description': robot_desc}
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[rsp_params, {'use_sim_time': use_sim_time}]),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(get_package_share_directory('buddy_description'), 'rviz', 'buddy.rviz')])
+    ])
