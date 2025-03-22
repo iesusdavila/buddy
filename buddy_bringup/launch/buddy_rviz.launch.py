87ccf51a19310776b0001da703016cef65945cb5
Iesus Davila <iesusdavila@gmail.com>
2025-03-22 13:54:12 -0500

    Add screen plugin

--- buddy_bringup/launch/buddy_rviz.launch.py
+++ buddy_bringup/launch/buddy_rviz.launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
             executable='rviz2',
             name='rviz2',
             output='screen',
-            arguments=['-d', os.path.join(get_package_share_directory('buddy_description'), 'rviz', 'buddy.rviz')]),
+            arguments=['-d', os.path.join(get_package_share_directory('buddy_bringup'), 'config', 'buddy.rviz')]),
         Node(
             package="usb_cam",
             executable="usb_cam_node_exe",
